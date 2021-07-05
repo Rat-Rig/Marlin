6dc49eb2c5f6df2c102dbed9865db7b85b9d9aa8
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2021-07-06 01:06:24 +0200

    Fix printer name and authors

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(tomaski)" // Who made the changes. // todo
+#define STRING_CONFIG_H_AUTHOR "(tomaski, miklschmidt)" // Who made the changes. // todo
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -144,7 +144,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "V-Core Pro" // todo
+#define CUSTOM_MACHINE_NAME "V-Core 3" // todo
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
